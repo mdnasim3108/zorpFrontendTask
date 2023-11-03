fa49fb54c021ffe759772a3db0cbc6e7f5df6390
md nasim <mohamednasim3108@gmail.com>
2023-11-03 23:55:59 +0530

    navbar

--- src/components/mobileNav.js
+++ src/components/mobileNav.js
@@ -13,13 +13,15 @@ import {
   faPlayCircle,
   faMessage,
   faUser,
-  faCircleXmark
+  faCircleXmark,
 } from "@fortawesome/free-regular-svg-icons";
 import { PiWebhooksLogoBold } from "react-icons/pi";
 import Settings from "./settings";
 import { useState } from "react";
+import { AiOutlineLeftCircle } from "react-icons/ai";
+
 const MobileNav = (props) => {
-  const [showNav,setShowNav]=useState(false)
+  const [showNav, setShowNav] = useState(false);
   const settings = [
     "Project",
     "API Access",
@@ -30,11 +32,20 @@ const MobileNav = (props) => {
     "Audit Logs",
     "Team Members",
   ];
+  const [set, showSet] = useState(false);
   return (
     <div className="w-full h-full  lg:hidden block">
-      <div className={`fixed  flex bg-white w-[70%] h-full z-20 border-r-2 border-r-gray-200 ${!showNav?"left-[-999px]":"left-0"} transition-all duration-300 ease-in-out`}>
+      <div
+        className={`fixed  flex bg-white ${
+          set ? "w-[70%]" : "w-[18%]"
+        } h-full z-20  border-r-gray-200 ${
+          !showNav ? "left-[-999px]" : "left-0"
+        } transition-all duration-300 ease-in-out`}
+      >
         <div
-          className={` w-[20%]  relative  border-r-2 border-r-gray-300 h-full flex flex-col items-center justify-between  py-3`}
+          className={` ${
+            !set ? "w-full" : "w-[20%] border-r-gray-300"
+          }  relative  border-r-2  h-full flex flex-col items-center justify-between  py-3`}
         >
           <img src={zorpLogo} className=" w-[3rem]" />
           <div className="bg-black flex items-center justify-center h-[2rem] w-[2rem] rounded-lg py-[0.5vh] font-semibold text-white lg:w-full">
@@ -61,6 +72,7 @@ const MobileNav = (props) => {
           <FontAwesomeIcon
             icon={faSliders}
             className="lg:w-full h-[1.5rem] text-gray-800 w-[3rem] "
+            onClick={() => showSet(true)}
           />
           <FontAwesomeIcon
             icon={faBook}
@@ -76,28 +88,32 @@ const MobileNav = (props) => {
           />
         </div>
 
-        <div className=" w-[80%] bg-white h-full lg:border-r-2 border-r-0 lg:border-b-0 border-b-2 flex flex-col items-start pl-5 pt-[4rem] relative">
-          <h1 className="font-bold text-2xl ">Settings</h1>
-          <div className="flex flex-col  h-[50%] w-full items-start  justify-between">
-            {settings.map((setting) => (
-              <div className="w-full  text-left py-1 pl-2 pr-10 hover:bg-gray-100 transition-all duration-200 ease-in-out cursor-pointer rounded">
-                <p className="text-sm font-bold font-inter">{setting}</p>
-              </div>
-            ))}
+        {set && (
+          <div className=" w-[80%] bg-white h-full lg:border-r-2 border-r-0 lg:border-b-0 border-b-2 flex flex-col items-start pl-5 pt-[4rem] relative">
+            <h1 className="font-bold text-2xl ">Settings</h1>
+            <div className="flex flex-col  h-[50%] w-full items-start  justify-between">
+              {settings.map((setting) => (
+                <div className="w-full  text-left py-1 pl-2 pr-10 hover:bg-gray-100 transition-all duration-200 ease-in-out cursor-pointer rounded">
+                  <p className="text-sm font-bold font-inter">{setting}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
-        <FontAwesomeIcon icon={faCircleXmark} className="absolute top-5 right-5 text-gray-500 w-[2rem] h-[2rem]" onClick={()=>setShowNav(false)}/>
-
+        {set && <FontAwesomeIcon
+          icon={faCircleXmark}
+          className="absolute top-5 right-5 text-gray-500 w-[2rem] h-[2rem]"
+          onClick={() => showSet(false)}
+        />}
       </div>
-
 
       <div className="w-full h-[7vh] bg-white shadow-lg fixed lg:hidden z-10 pr-10 flex justify-between lg:justify-end">
         <div className={` my-auto ml-5`}>
           <button
             type="button"
             className="text-gray-700 focus:outline-none "
-            onClick={()=>setShowNav(true)}
+            onClick={() => setShowNav(true)}
           >
             <svg
               className="h-6 w-6"
@@ -116,7 +132,12 @@ const MobileNav = (props) => {
         </div>
       </div>
 
-      {showNav && <div className="bg-black opacity-[0.5] w-full h-[200vh] absolute z-10" onClick={()=>setShowNav(false)}></div>}
+      {showNav && (
+        <div
+          className="bg-black opacity-[0.5] w-full h-[200vh] absolute z-10"
+          onClick={() => setShowNav(false)}
+        ></div>
+      )}
     </div>
   );
 };
--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -16,7 +16,6 @@ import {
 } from "@fortawesome/free-regular-svg-icons";
 import {PiWebhooksLogoBold} from "react-icons/pi"
 const Sidebar = (props) => {
-  console.log(window.innerWidth);
 
   return (
     <div className={`lg:w-[4rem] w-full    lg:h-full h-[5rem] lg:border-r-2 lg:border-b-0 border-b-2 items-center  lg:flex hidden lg:flex-col  lg:justify-between   py-6 relative`}>
@@ -46,7 +45,9 @@ const Sidebar = (props) => {
         <PiWebhooksLogoBold className="w-[1.8rem] h-[1.8rem]  ml-7 lg:ml-0"/>
         <FontAwesomeIcon
           icon={faSliders}
-          className="lg:w-full h-[1.5rem] text-gray-800 w-[10%]"
+          className={`lg:w-full h-[1.5rem] text-gray-800 w-[10%] ${props.showSettings && "text-violet-600"} cursor-pointer`}
+          onClick={props.open}
+
         />
         <FontAwesomeIcon
           icon={faBook}
@@ -95,9 +96,7 @@ const Sidebar = (props) => {
         <img src={userImg} className="lg:w-[3rem]  h-[2rem] rounded-full w-[2rem]"/> */}
      
 
-      {!props.showSettings && <AiOutlineRightCircle className="text-gray-200 w-[1.5rem] h-[1.5rem] absolute lg:top-10 lg:right-[-13px] bottom-[-1rem] right-[50%] z-10 bg-white cursor-pointer"
-        onClick={props.open}
-        />}
+      
     </div>
   );
 };
